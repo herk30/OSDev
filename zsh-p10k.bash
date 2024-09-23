ae145acb75322ebae7fa15d67fcd3a3e97b3cd50
Lele <78266397+herk30@users.noreply.github.com>
2024-09-23 10:35:21 +0700

    Up ZSH Bash

--- zsh-p10k.bash
+++ zsh-p10k.bash
@@ -7,7 +7,7 @@ sudo apt install curl
 sudo apt install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # For zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
