3d2f6ac4f6b707633b3e1c91566e3de4fe88877a
Lele <Lekhoinguyen1030@gmail.com>
2024-10-21 11:06:51 +0700

    Add existing file

--- boot.asm
+++ boot.asm
@@ -1,8 +1,9 @@
-.set ALIGN,    1<<0          
-.set MEMINFO,  1<<1             
-.set FLAGS,    ALIGN | MEMINFO 
-.set MAGIC,    0x1BADB002       
-.set CHECKSUM, -(MAGIC + FLAGS) 
+/* Declare constants for the multiboot header. */
+.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
+.set MEMINFO,  1<<1             /* provide memory map */
+.set FLAGS,    ALIGN | MEMINFO  /* this is the Multiboot 'flag' field */
+.set MAGIC,    0x1BADB002       /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS) /* checksum of above, to prove we are multiboot */
 
 .section .multiboot
 .align 4
@@ -20,13 +21,9 @@ stack_top:
 .global _start
 .type _start, @function
 _start:
-
 	mov $stack_top, %esp
-
 	call kernel_main
-
 	cli
 1:	hlt
 	jmp 1b
-
 .size _start, . - _start
